b6256bfb726b354d27203c85a466ee2a27416e58
Tomas1207 <tomasfernandes@gmail.com>
2020-06-05 14:39:08 +0100

    Main refactor

--- PTsimsColetes/main.py
+++ PTsimsColetes/main.py
@@ -29,12 +29,13 @@ def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=
 
 nomes = ['Tomas1207','Mr Rato','Andre','TheBigOne','Stone Ricci','Rui_Sludge','Mata Pombos','Kaos','Crisostomo','MiTraPT','MacgyverPT','Dog','Costa','SrWer','MaG','NicoFuzu','Krespin','Rodriguez','Batman','Ranger','Birta','Telmo','Red']
 types = ['Desert','DPM']
+#nomes = ['Pedro7161']
 #x= 986/1173
 #y= 1229/1265
 #x1 = 988/1171
 #y1 = 1228/1266
 userinput = ""
-counter = 0
+
 total = len(nomes)
 
 def namecontrol(name):
@@ -47,146 +48,96 @@ def namecontrol(name):
 def mirroImg(image):
     return ImageOps.mirror(image)
  
-
-def textdrawonimg(mirro,nome,font_type):
+def seralizenome(nome):
+    return nome.replace(" ","").lower()
+def textdrawonimg(mirro,nome,tipo,font_type):
     draw = ImageDraw.Draw(mirro)
     text = draw.textsize(nome[::-1],font_type)
     draw.text(xy=(((1060+874)/2)-text[0]/2,((1267+1227)/2)-text[1]/2),text=nome,font=font_type)
-    nome = nome.replace(" ","").lower()
-    mirro.save("mirrorImagens/PT_DPM_Desert_"+ nome +"_vests.png")
+    nome = seralizenome(nome)
+    mirro.save("mirrorImagens/PT_DPM_"+tipo+"_"+ nome +"_vests.png")
 
 def opentypeimg(tipo):
-    return Image.open(f'.\Image\{type}.jpg')
+    return Image.open(f'.\Image\{tipo}.jpg')
 
 def unmirroandreadytopack(tipo,nome):
+    nome = seralizenome(nome)
     imagepos = Image.open("mirrorImagens/PT_DPM_"+tipo+"_"+ nome +"_vests.png")
     img = mirroImg(imagepos)
     img.save("FinalImagens/PT_DPM_"+tipo+"_"+ nome + "_vests.png")
 
 def imgageCretator(tipo):
-
+    counter = 0
     for nome in nomes:
         counter +=1
         printProgressBar(counter,total,prefix="Progress:",suffix='Complete', length=50)
-        textdrawonimg(mirroImg(opentypeimg(tipo)),nome,namecontrol(nome))
+        textdrawonimg(mirroImg(opentypeimg(tipo)),nome,tipo,namecontrol(nome))
         unmirroandreadytopack(tipo,nome)
+        nome = seralizenome(nome)
         driveinfo = subprocess.Popen(f'powershell.exe ..\ImageToPAA\ImageToPAA.exe ..\\PTsimsColetes\\FinalImagens\\PT_DPM_{tipo}_{nome}_vests.png ..\\Mod\\addons\\pt_items\\data\\DPM\\PT_DPM_{tipo}_{nome}_vests.paa',stdout=subprocess.DEVNULL)
-    
+        writeconfig(nome,tipo)
         
-        #driveinfo = subprocess.Popen(f'powershell.exe ..\ImageToPAA\ImageToPAA.exe ..\\PTsimsColetes\\FinalImagens\\PT_DPM_{nome}_vests.png ..\\Mod\\addons\\pt_items\\data\\DPM\\PT_DPM_{nome}_vests.paa',stdout=subprocess.DEVNULL)
 
-def writeconfig(nome):
+def writeconfig(nome,tipo):
+    nome = seralizenome(nome)
     with open('..\\Mod\\addons\\pt_items\\config.cpp','r+')as config:
         lines = config.readlines()
         for line in lines:
-            if userinput == "desert":
-                if "PTc Desert "+nome+" Plate Carrier" in line:
+                if f"PTc {tipo} {nome} Plate Carrier" in line:
                     break
-            else:
-                if "PTc "+nome+" Plate Carrier" in line:
-                    break
-        else:
-            if userinput == "desert":
-                template = """};\nclass DS_PT_Desert_"""+nome+"""_Plate_Carrier: V_PlateCarrier1_rgr {
-            displayName = "PTc Desert """+nome+""" Plate Carrier ";
-            picture = "\PT_items\data\DPM_Desert\Icon_PT_DPM_Desert_vests.paa";
-            hiddenSelections[] = {
-                "camo"
-            };
-            hiddenSelectionsTextures[] = {
-                "\PT_items\data\DPM\PT_DPM_Desert_"""+nome+"""_vests.paa"
-            };
-            class ItemInfo: vestitem {
-                uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02.p3d";
-                containerclass = "Supply120";
-                mass = 50;
-                hiddenSelections[] = {
-                    "camo"
-                };
-                class HitpointsProtectionInfo {
-                    class Neck {
-                        hitpointName = "HitNeck";
-                        armor = 8;
-                        passThrough = 0.5;
-                    };
-                    class Arms {
-                        hitpointName = "HitArms";
-                        armor = 8;
-                        passThrough = 0.5;
-                    };
-                    class Chest {
-                        hitpointName = "HitChest";
-                        armor = 24;
-                        passThrough = 0.1;
-                    };
-                    class Diaphragm {
-                        hitpointName = "HitDiaphragm";
-                        armor = 24;
-                        passThrough = 0.1;
-                    };
-                    class Abdomen {
-                        hitpointName = "HitAbdomen";
-                        armor = 24;
-                        passThrough = 0.1;
-                    };
-                    class Body {
-                        hitpointName = "HitBody";
-                        passThrough = 0.1;
-                    };
-                };
-            };
-        };"""
-            else:
-                template = """};\nclass DS_PT_"""+nome+"""_Plate_Carrier: V_PlateCarrier1_rgr {
-            displayName = "PTc """+nome+""" Plate Carrier ";
-            picture = "\PT_items\data\DPM\Icon_PT_DPM_vests.paa";
-            hiddenSelections[] = {
-                "camo"
-            };
-            hiddenSelectionsTextures[] = {
-                "\PT_items\data\DPM\PT_DPM_"""+nome+"""_vests.paa"
-            };
-            class ItemInfo: vestitem {
-                uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02.p3d";
-                containerclass = "Supply120";
-                mass = 50;
-                hiddenSelections[] = {
-                    "camo"
-                };
-                class HitpointsProtectionInfo {
-                    class Neck {
-                        hitpointName = "HitNeck";
-                        armor = 8;
-                        passThrough = 0.5;
-                    };
-                    class Arms {
-                        hitpointName = "HitArms";
-                        armor = 8;
-                        passThrough = 0.5;
-                    };
-                    class Chest {
-                        hitpointName = "HitChest";
-                        armor = 24;
-                        passThrough = 0.1;
-                    };
-                    class Diaphragm {
-                        hitpointName = "HitDiaphragm";
-                        armor = 24;
-                        passThrough = 0.1;
-                    };
-                    class Abdomen {
-                        hitpointName = "HitAbdomen";
-                        armor = 24;
-                        passThrough = 0.1;
-                    };
-                    class Body {
-                        hitpointName = "HitBody";
-                        passThrough = 0.1;
-                    };
-                };
-            };
-        };"""
            
+        else:
+            
+            template = """};\nclass DS_PT_"""+tipo+"""_"""+nome+"""_Plate_Carrier: V_PlateCarrier1_rgr {
+            displayName = "PTc """+tipo+""" """+nome+""" Plate Carrier ";
+            picture = "\PT_items\data\DPM\Icon_PT_DPM_"""+tipo+"""_vests.paa";
+            hiddenSelections[] = {
+                "camo"
+            };
+            hiddenSelectionsTextures[] = {
+                "\PT_items\data\DPM\PT_DPM_"""+tipo+"""_"""+nome+"""_vests.paa"
+            };
+            class ItemInfo: vestitem {
+                uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02.p3d";
+                containerclass = "Supply120";
+                mass = 50;
+                hiddenSelections[] = {
+                    "camo"
+                };
+                class HitpointsProtectionInfo {
+                    class Neck {
+                        hitpointName = "HitNeck";
+                        armor = 8;
+                        passThrough = 0.5;
+                    };
+                    class Arms {
+                        hitpointName = "HitArms";
+                        armor = 8;
+                        passThrough = 0.5;
+                    };
+                    class Chest {
+                        hitpointName = "HitChest";
+                        armor = 24;
+                        passThrough = 0.1;
+                    };
+                    class Diaphragm {
+                        hitpointName = "HitDiaphragm";
+                        armor = 24;
+                        passThrough = 0.1;
+                    };
+                    class Abdomen {
+                        hitpointName = "HitAbdomen";
+                        armor = 24;
+                        passThrough = 0.1;
+                    };
+                    class Body {
+                        hitpointName = "HitBody";
+                        passThrough = 0.1;
+                    };
+                };
+            };
+        };"""
+
             del lines[-1]
             lines[-1] = template
             #config.write(test)  
@@ -194,3 +145,5 @@ def writeconfig(nome):
                 for line in lines:
                     cenas.write(line)
                 cenas.write('\n};')
+for tipo in types:
+    imgageCretator(tipo)
